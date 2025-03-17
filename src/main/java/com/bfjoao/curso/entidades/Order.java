d9082f275abd71c90669cbafc00edf188cdd2925
João Barbosa <joao.bffff@gmail.com>
2025-03-17 16:01:11 -0300

    OrderStatus enum

--- src/main/java/com/bfjoao/curso/entidades/Order.java
+++ src/main/java/com/bfjoao/curso/entidades/Order.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
-import org.hibernate.annotations.ManyToAny;
+import com.bfjoao.curso.entidades.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -25,6 +25,8 @@ public class Order implements Serializable {
 	private Long id;
 	private Instant moment;
 
+	private Integer orderStatus;
+	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
@@ -33,10 +35,11 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -63,7 +66,17 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
+	
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
 
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+	}
+}
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
