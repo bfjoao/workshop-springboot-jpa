102f6555783280f7a68a1226cb7b16e3bcb6d7ab
João Pedro Barbosa Fernandes <joao-fernandes@REDERS>
2025-03-21 11:17:55 -0300

    User update

--- src/main/java/com/bfjoao/curso/resources/UserResource.java
+++ src/main/java/com/bfjoao/curso/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.bfjoao.curso.entidades.User;
 import com.bfjoao.curso.services.UserService;
+
 
 
 @RestController // Transforma esta classe em um controlador REST do Spring Boot.
@@ -48,5 +50,11 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id){
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj){
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 }
